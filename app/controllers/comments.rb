3049133dd53f339c83a316c0ebaabf255b275767
xiranou <xiranou@gmail.com>
2014-11-12 14:37:55 -0500

    add post route for new comments

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -3,3 +3,9 @@ get '/review/:id/comment/new' do |id|
 
   erb :"comments/_new", locals:{review: @review}
 end
+
+post '/review/:id/comment' do
+  @comment = Comment.create(params[:comment])
+
+  redirect "/review/#{@comment.review.id}"
+end
