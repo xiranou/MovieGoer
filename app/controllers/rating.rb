d7836991d8d86db2e017f76e5d224fac516e0ae3
xiranou <xiranou@gmail.com>
2014-11-12 18:51:54 -0500

    add ratings to movies

--- app/controllers/rating.rb
+++ app/controllers/rating.rb
@@ -1,7 +1,7 @@
 post '/rating' do
   @movie = Movie.find(params[:rateable_id])
   @voter = User.find(params[:rateable_id])
-  Rating.create(score: params[:score], rateable: @movie, voter: @voter)
+  Rating.create(score: params[:score].to_i, rateable: @movie, voter: @voter)
 
   redirect '/movies'
 end
