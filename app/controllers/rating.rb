51dd12c15c28c0bbf2ae20895fa647fef6fed8f9
xiranou <xiranou@gmail.com>
2014-11-12 21:06:16 -0500

    fix voter id bug on rating rout

--- app/controllers/rating.rb
+++ app/controllers/rating.rb
@@ -1,6 +1,6 @@
 post '/rating' do
   @movie = Movie.find(params[:rateable_id])
-  @voter = User.find(params[:rateable_id])
+  @voter = User.find(params[:voter_id])
   Rating.create(score: params[:score].to_i, rateable: @movie, voter: @voter)
 
   redirect '/movies'
