7be72bcaafe978068d3111e1bfed1e52a7408710
xiranou <xiranou@gmail.com>
2014-11-12 19:22:59 -0500

    average_score now rounds to two decimal

--- app/helpers/average_score.rb
+++ app/helpers/average_score.rb
@@ -1,4 +1,4 @@
 def average_score(object)
   total_scores = object.ratings.map { |rating| rating.score }
-  return total_scores.inject(:+).to_f/object.ratings.size
+  return (total_scores.inject(:+).to_f/object.ratings.size).round(1)
 end
