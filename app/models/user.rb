045afdc0bfe450383052c0c312fbb36899cfbe2c
xiranou <xiranou@gmail.com>
2014-11-14 16:55:06 -0500

    fix polymorphic table

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,10 @@ class User < ActiveRecord::Base
   has_many :watched_movies, through: :reviews, source: :movie
   has_many :comments, foreign_key: :commenter_id
 
-  has_many :ratings, as: :rateable, foreign_key: :voter_id
-  # has_many :rated_movies, through: :ratings, source: :rateable, source_type: "Movie", foreign_key: :voter_id
-  # has_many :rated_reviews, through: :ratings, source: :rateable, source_type: "Review", foreign_key: :voter_id
-  # has_many :rated_comments, through: :ratings, source: :rateable, source_type: "Comment", foreign_key: :voter_id
+  has_many :ratings, foreign_key: :voter_id
+
+  has_many :rated_movies, through: :ratings, source: :rateable, source_type: "Movie", foreign_key: :voter_id
+  has_many :rated_reviews, through: :ratings, source: :rateable, source_type: "Review", foreign_key: :voter_id
+  has_many :rated_comments, through: :ratings, source: :rateable, source_type: "Comment", foreign_key: :voter_id
+  has_many :rated_users, through: :ratings, source: :rateable, source_type: "User", foreign_key: :voter_id
 end
